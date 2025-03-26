e6cde633cd18818fdaa1a4135ffbb016aee2234e
A31Nesta <anthroponesta@gmail.com>
2025-03-26 21:38:07 +0100

    implement basic range-based generator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use std::error::Error;
+
 use grand::rng::Randomizable;
 
-fn main() {
-    println!("Random f64: {}", f64::random());
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut has_min = false;
+    let mut has_max = false;
+
+    const MIN: u16 = 32;
+    const MAX: u16 = 42069;
+
+    loop {
+        let res = u16::random_range(MIN, MAX)?;
+        print!("{res}, ");
+        if *res == MAX { has_max = true }
+        if *res == MIN { has_min = true }
+        if has_max && has_min { break }
+        
+        if *res < MIN { panic!() }
+        if *res > MAX { panic!() }
+    }
+
+    let res = u16::random_range(MIN, MAX)?;
+    println!("{res}");
+    println!("Done!");
+
+    Ok(())
 }
--- src/rng.rs
+++ src/rng.rs
@@ -1,5 +1,30 @@
-pub trait Randomizable {
+use std::{error::Error, fmt::{Debug, Display}};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct RangeError<T: Display + Debug> {
+    should_be_smaller: T,
+    should_be_bigger: T
+}
+impl<T: Display + Debug> RangeError<T> {
+    pub fn new(should_be_smaller: T, should_be_bigger: T) -> Self {
+        RangeError {
+            should_be_smaller: should_be_smaller,
+            should_be_bigger: should_be_bigger
+        }
+    }
+}
+impl<T: Display + Debug> Display for RangeError<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid range, x ({}) should be smaller than y ({})", self.should_be_smaller, self.should_be_bigger)
+    }
+}
+impl<T: Display + Debug> Error for RangeError<T> {}
+
+type RangeResult<T> = Result<Box<T>, RangeError<Box<T>>>;
+
+pub trait Randomizable: Display + Debug {
     fn random() -> Self;
+    fn random_range(x: Self, y: Self) -> RangeResult<Self>;
 }
 
 // BYTE
@@ -9,12 +34,40 @@ impl Randomizable for u8 {
         getrandom::fill(&mut number).expect("getting random numbers should be possible");
         number[0]
     }
+    
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        res = res % (corrected_diff) + x;
+
+        Ok(Box::new(res))
+    }
+    
 }
 impl Randomizable for i8 {
     fn random() -> Self {
         let mut buf = [0u8];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         i8::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        let offset = if res < 0 { y } else { x };
+        res = res % (corrected_diff) + offset;
+
+        Ok(Box::new(res))
     }
 }
 // SHORT
@@ -24,12 +77,39 @@ impl Randomizable for u16 {
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         u16::from_ne_bytes(buf)
     }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        res = res % (corrected_diff) + x;
+
+        Ok(Box::new(res))
+    }
 }
 impl Randomizable for i16 {
     fn random() -> Self {
         let mut buf = [0u8; 2];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         i16::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        let offset = if res < 0 { y } else { x };
+        res = res % (corrected_diff) + offset;
+
+        Ok(Box::new(res))
     }
 }
 // INT
@@ -39,12 +119,39 @@ impl Randomizable for u32 {
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         u32::from_ne_bytes(buf)
     }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        res = res % (corrected_diff) + x;
+
+        Ok(Box::new(res))
+    }
 }
 impl Randomizable for i32 {
     fn random() -> Self {
         let mut buf = [0u8; 4];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         i32::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        let offset = if res < 0 { y } else { x };
+        res = res % (corrected_diff) + offset;
+
+        Ok(Box::new(res))
     }
 }
 // LONG
@@ -54,12 +161,39 @@ impl Randomizable for u64 {
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         u64::from_ne_bytes(buf)
     }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        res = res % (corrected_diff) + x;
+
+        Ok(Box::new(res))
+    }
 }
 impl Randomizable for i64 {
     fn random() -> Self {
         let mut buf = [0u8; 8];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         i64::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        let offset = if res < 0 { y } else { x };
+        res = res % (corrected_diff) + offset;
+
+        Ok(Box::new(res))
     }
 }
 // COMICALLY LARGE
@@ -69,12 +203,39 @@ impl Randomizable for u128 {
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         u128::from_ne_bytes(buf)
     }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        res = res % (corrected_diff) + x;
+
+        Ok(Box::new(res))
+    }
 }
 impl Randomizable for i128 {
     fn random() -> Self {
         let mut buf = [0u8; 16];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         i128::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let corrected_diff = if diff == Self::MAX { diff } else { diff+1 };
+        let mut res = Self::random();
+        let offset = if res < 0 { y } else { x };
+        res = res % (corrected_diff) + offset;
+
+        Ok(Box::new(res))
     }
 }
 
@@ -85,11 +246,35 @@ impl Randomizable for f32 {
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         f32::from_ne_bytes(buf)
     }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let mut res = Self::random();
+        res = res % (diff+1f32) + x;
+
+        Ok(Box::new(res))
+    }
 }
 impl Randomizable for f64 {
     fn random() -> Self {
         let mut buf = [0u8; 8];
         getrandom::fill(&mut buf).expect("getting random numbers should be possible");
         f64::from_ne_bytes(buf)
+    }
+
+    fn random_range(x: Self, y: Self) -> RangeResult<Self> {
+        if x > y {
+            return Err(RangeError::new(Box::new(x), Box::new(y)));
+        }
+
+        let diff = y - x;
+        let mut res = Self::random();
+        res = res % (diff+1f64) + x;
+
+        Ok(Box::new(res))
     }
 }
