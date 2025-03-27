8a22ce83da3ed5c57ccb11d1422f2768e3823df6
a31nesta <anthroponesta@gmail.com>
2025-03-27 15:31:27 +0100

    try to create random float by creating a random i128 and fail terribly

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
 pub mod rng;
+
+use rng::Randomizable;
+
+pub fn random_f64() -> f64 {
+    // const MIN: u64 = i64::MIN as u64;
+    // const MAX: u64 = i64::MAX as u64;
+
+    let rand_base = i128::random();
+    let range = u64::MAX;
+
+    let proportion_unit = u128::MAX / range as u128;
+
+    let proportional_rand_int = rand_base / proportion_unit as i128;
+    let proportional_rand_remainder = rand_base % proportion_unit as i128;
+    let proportional_rand_decimals = 1f64 / proportional_rand_remainder as f64;
+
+    proportional_rand_int as f64 + proportional_rand_decimals
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 use std::error::Error;
 
-use grand::rng::Randomizable;
+use grand::random_f64;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut has_min = false;
-    let mut has_max = false;
-
-    const MIN: u16 = 32;
-    const MAX: u16 = 42069;
-
-    loop {
-        let res = u16::random_range(MIN, MAX)?;
-        print!("{res}, ");
-        if *res == MAX { has_max = true }
-        if *res == MIN { has_min = true }
-        if has_max && has_min { break }
-        
-        if *res < MIN { panic!() }
-        if *res > MAX { panic!() }
-    }
-
-    let res = u16::random_range(MIN, MAX)?;
-    println!("{res}");
-    println!("Done!");
+    let num = random_f64();
+    println!("{num}");
 
     Ok(())
 }
